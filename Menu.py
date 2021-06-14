8634c0cdd467494225dd9a8665e251503a057cb6
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-06-14 15:57:05 -0500

    Removed unecessary files

--- Menu.py
+++
@@ -1,3 +0,0 @@
-import os 
-
-os.close('Music.app')
--- command.py
+++
@@ -1,6 +0,0 @@
-import subprocess  
-
-list_files = subprocess.run('ls')
-home = subprocess.run(['cd', '~'])
-list_files = subprocess.run('ls')
-
