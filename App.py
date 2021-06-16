64b5ba9084f40982f0342f295651343af3cd562b
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-06-15 22:58:02 -0500

    Final project is completed. Everything is working and ready for presentation.

--- App.py
+++ App.py
@@ -8,29 +8,19 @@ class App:
         self.app_name = app_name
         self.system_app = system_app
 
-    #? os acts like a terminal, in () is what is inserted to the makeshift terminal
-    #? this is a path to first half of all applications 
     def open_appSystem(self):
         os.system(f"open /System/Applications/{self.app_name}.app")
         opened_System.append(f'{self.app_name}')
-        # print(opened_System)
         return opened_System
 
-    #? this is another path to the second half of applications         
     def open_app(self):
         if self.system_app:
             self.open_appSystem()
         else:
             os.system(f'open /Applications/{self.app_name}.app')
             opened.append(f'{self.app_name}')
-            # print(opened)
             return opened
 
-    # # #? this is to close any application located under System
-    # def close_appSystem(self):
-            os.system(f'killall {self.app_name}')
-            
-    #?to close an application located under Applications
     def close_app(self):
             os.system(f"killall {self.app_name}")
     
--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 
-from App import App
+from app import App
 import os
 # TODO need to make a READ ME 
 
-#what did you learn
-#show the code
-
-# ? these apps are for open_app
 headspace = App('Headspace')
 safari = App('Safari')
 slack = App('Slack')
-
-# ? these apps are for open_app
 music = App('Music', True)
 calendar = App('Calender', True)
 launchpad = App('Launchpad', True)
@@ -19,9 +13,10 @@ messages = App('Messages', True)
 notes = App('Notes', True)
 stickies = App('Stickies', True)
 stocks = App('Stocks', True)
-all_apps = [music, safari, calendar, messages, notes, stickies, stocks,
-    headspace, safari, slack]
-#  * create the menu for the routine
+
+all_apps = [music, safari, calendar, messages, notes,
+stickies, stocks, headspace, safari, slack]
+
 while True:
         print('\033[32m' + '''
 1) School
@@ -31,80 +26,143 @@ while True:
         print('\033[4;31m' + '5) Exit\n' + '\033[0;39m')
         des = input('Number: ')
         if des == '1':
-            safari.open_app()
-            slack.open_app()
+            yay0nay = input('\033[32m' + '\nOpen apps? (y/n): '+'\033[0;39m')
+            if yay0nay == 'y':
+                safari.open_app()
+                slack.open_app()
             des = input('\033[32m'+'\nWant the launchpad?(y/n): ')
             if des == 'y':
                 launchpad.open_app()             
             while True:
-                print('''
-0) Go back
-1) Close School Apps
-''')
-                des = input('\033[0;39m'+'Number: ')
+                print('\033[32m'+'\n0) Go back')
+                print('\033[31m'+"1) Any apps you don't want to close?")
+                print('\033[4;31m'+'2) Close School Apps' + '\033[0;39m')
+                des = input('\033[0;39m'+'\nNumber: ')
+                print(' ')
                 if des == '0':
                     break
                 elif des == '1':
+                    name_of_app = []
+                    print(' ')
+                    for app in all_apps:
+                        title = app.app_name
+                        print('\033[33m'+title, end = ' | ' + '\033[0;39m')
+                        name_of_app.append(title)
+                    app_des = input('\033[36m'+'\nWhich app should I leave open?: '+ '\033[0;39m')
+                    name_of_app.remove(app_des)
+                    while True:
+                        print()
+                        y0n = input('\033[32m'+'Any other app? (y/n): '+ '\033[0;39m')
+                        if y0n == 'n':
+                            for app in name_of_app:
+                                os.system(f'killall {app}')
+                            break
+                        else:
+                            app_des = input('\nWhat other app?: ')
+                            name_of_app.remove(app_des)
+                            continue
+                elif des == '2':
                     #close apps 
                     safari.close_app()
                     slack.close_app()
                     break
-
         elif des == '2':
-            safari.open_app()
-            headspace.open_app()
-            music.open_app()
-            messages.open_app()
-            stickies.open_app()
+            yay0nay = input('\033[32m' + '\nOpen apps? (y/n): '+'\033[0;39m')
+            if yay0nay == 'y':
+                safari.open_app()
+                headspace.open_app()
+                music.open_app()
+                messages.open_app()
+                stickies.open_app()
             while True:
-                print('\033[32m'+'''
-0) Go back
-1) Close After School Apps''')
+                print('\033[32m'+'\n0) Go back')
+                print('\033[31m'+"1) Any apps you don't want to close?")
+                print('\033[4;31m'+'2) Close After School Apps' + '\033[0;39m')
                 des = input('\033[0;39m'+'\nNumber: ')
                 if des == '0':
                     break
-                elif des == '1':                    
+                if des == '1':
+                    name_of_app = []
+                    print(' ')
+                    for app in all_apps:
+                        title = app.app_name
+                        print('\033[33m'+title, end = ' | ' + '\033[0;39m')
+                        name_of_app.append(title)
+                    app_des = input('\033[36m'+'\nWhich app should I leave open?: '+ '\033[0;39m')
+                    name_of_app.remove(app_des)
+                    while True:
+                        print()
+                        y0n = input('\033[32m'+'\nAny other app? (y/n): '+ '\033[0;39m')
+                        if y0n == 'n':
+                            for app in name_of_app:
+                                os.system(f'killall {app}')
+                            break
+                        else:
+                            app_des = input('\nWhat other app?: ')
+                            name_of_app.remove(app_des)
+                            continue
+                elif des == '2':                    
                     #close apps 
                     safari.close_app()
                     headspace.close_app()
                     messages.close_app()
                     stickies.close_app()
                     break
-
         elif des == '3':
-            safari.open_app()
-            messages.open_app()
-            music.open_app()
+            yay0nay = input('\033[32m' + '\nOpen apps? (y/n): '+'\033[0;39m')
+            if yay0nay == 'y':
+                safari.open_app()
+                messages.open_app()
+                music.open_app()
             while True:
-                print('\033[32m'+'''
-0) Go back
-1) Close Resting Apps''')
+                print('\033[32m'+'\n0) Go back')
+                print('\033[31m'+"1) Any apps you don't want to close?")
+                print('\033[4;31m'+'2) Close Resting Apps' + '\033[0;39m')
                 des = input('\033[0;39m'+'\nNumber: ')
                 if des == '0':
                     break
-                elif des == '1':
+                if des == '1':
+                    name_of_app = []
+                    print(' ')
+                    for app in all_apps:
+                        title = app.app_name
+                        print('\033[33m'+title, end = ' | ' + '\033[0;39m')
+                        name_of_app.append(title)
+                    app_des = input('\033[36m'+'\nWhich app should I leave open?: '+ '\033[0;39m')
+                    name_of_app.remove(app_des)
+                    while True:
+                        print()
+                        y0n = input('\033[32m'+'Any other app? (y/n): '+ '\033[0;39m')
+                        if y0n == 'n':
+                            for app in name_of_app:
+                                os.system(f'killall {app}')
+                            break
+                        else:
+                            app_des = input('\nWhat other app?: ')
+                            name_of_app.remove(app_des)
+                            continue
+                elif des == '2':
                     safari.close_app()
                     messages.close_app()
                     music.close_app()
-
         elif des == '4':
-
             while True:
-                print('\033[32m'+'0) Go back')
+                print('\033[32m'+'\n0) Go back')
                 print('\033[31m'+"1) Any apps you don't want to close?")
                 print('\033[1;4;31m'+'2) Close All' + '\033[0;39m')
                 des = input('\nNumber: ')
                 if des == '1':
                     name_of_app = []
+                    print(' ')
                     for app in all_apps:
                         title = app.app_name
-                        print(title, end = ' | ')
+                        print('\033[33m'+title, end = ' | ' + '\033[0;39m')
                         name_of_app.append(title)
-                        # print(name_of_app)
-                    app_des = input('\nWhich app should I leave open?: ')
+                    app_des = input('\033[36m'+'\nWhich app should I leave open?: '+ '\033[0;39m')
                     name_of_app.remove(app_des)
                     while True:
-                        y0n = input('\nAny other app? (y/n): ')
+                        print()
+                        y0n = input('\033[32m'+'Any other app? (y/n): '+ '\033[0;39m')
                         if y0n == 'n':
                             for app in name_of_app:
                                 os.system(f'killall {app}')
